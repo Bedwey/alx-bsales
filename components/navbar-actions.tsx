2ae7b12442ff252aca218974836f5641cd7e6033
Abdullah Bedwey <bedweyy@gmail.com>
2024-06-09 21:12:49 +0300

    chore: Add ModalProvider and ToastProvider to RootLayout finish cart  and add toaster ❤️🚀

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 import { ShoppingBag } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const NavbarActions = () => {
@@ -11,14 +13,17 @@ const NavbarActions = () => {
         setIsMounted(true);
     }, []);
 
+    const router = useRouter();
+    const cart = useCart();
+
     if (!isMounted) return null;
 
     return (
         <div className="ml-auto flex items-center gap-x-4">
-            <Button>
+            <Button onClick={() => router.push('/cart')}>
                 <ShoppingBag size={20} color="white" />
                 <span className="ml-2 text-sm font-medium text-white">
-                    0
+                    {cart.items.length}
                 </span>
             </Button>
         </div>
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -6,17 +6,35 @@ import IconButton from "@/components/ui/icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCardProps {
     data: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+    const cart = useCart();
+    const previewModal = usePreviewModal();
     const router = useRouter();
+
+
     const handleClick = () => {
         router.push(`/product/${data.id}`);
     }
 
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+
+        previewModal.onOpen(data);
+    };
+
+
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+        cart.addItem(data);
+    };
 
     // if (!data.products_images?.[0]) {
     //     return null;
@@ -34,11 +52,11 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
                         <IconButton
-                            onClick={() => console.log('Add to cart')}
+                            onClick={onPreview}
                             icon={<Expand size={20} className="text-gray-600" />}
                         />
                         <IconButton
-                            onClick={() => console.log('Add to cart')}
+                            onClick={onAddToCart}
                             icon={<ShoppingCart size={20} className="text-gray-600" />}
                         />
                     </div>
